0d73aa306621ba897d92b7532244e78e5586bc13
Joseph Atkins-Turkish <jat1g11@soton.ac.uk>
2015-02-19 17:39:24 +0000

    spelling

--- scraper/settings.py
+++ scraper/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for scraperr project
+# Scrapy settings for scraper project
 #
 # For simplicity, this file contains only the most important settings by
 # default. All the other settings are documented here:
